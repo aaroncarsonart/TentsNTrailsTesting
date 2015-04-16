7fff4d74d48b7df1f5d9ef659c15c0ad2c7a8630
Jared Petersen <jpetersen11@wou.edu>
2015-04-15 20:04:57 -0700

    Cleaned-up testing suite

--- TentsNTrailsTesting/src/com/tentsntrails/testing/sprint5/Main.java
+++ TentsNTrailsTesting/src/com/tentsntrails/testing/sprint5/Main.java
@@ -18,32 +18,68 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * Save this java file. Then copy and paste the file (minus package name)
  * in to a new Java class in this project. Now, add a new testing block,
  * as shown below.
+ * 
  * @author Cookie Computing
  *
  */
 public class Main {
+	
 	public static void main(String[] args) {
 		
 		SeleniumTutorialGuru stg = new SeleniumTutorialGuru();
 		
 		/**
 		 *TESTING BLOCK:
+		 *
 		 *In order to establish some consistency, please copy and
 		 *paste a whole try/catch block and then replace the variable's
 		 *class and print statement to match the new test's name.
-		 */
-        try {
+		 *
+		 *Please use the below example test case as your template.
+		 **/
+		
+		/** Example Test Case **/
+		// User Story #0 - Login and Logout as an Admin
+        try 
+        {
+        	LoginAsAdmin test = new LoginAsAdmin();
+        	test.setUp();
+        	test.testLoginAsAdmin();
+        	test.tearDown();
+        	System.out.println("[PASSED] #---: Login as Admin");
+		} 
+        catch (Exception e) 
+        {
+			// Test failed, print out error message
+			// "======" are there to make the failures more visible
+			System.out.println("=====================================================================");
+			System.out.println("[FAILED] #---: Login as Admin");
+			System.out.println("=====================================================================");
+			e.printStackTrace();
+		}
+		
+		/** Sprint 5 Test Cases **/
+		// User Story #168 -- Merge Locations
+        try 
+        {
         	MergeLocations test = new MergeLocations();
         	test.setUp();
         	test.testMergeLocations();
         	test.tearDown();
-        	System.out.println("[PASSED] MergeLocations"); //will only reach here if no Exception is thrown
-		} catch (Exception e) {
-			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");//just to catch our eyes
-			System.out.println("[FAILED] MergeLocations");
-			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        	System.out.println("[PASSED] #168: Merge Locations");
+		}
+        catch (Exception e) 
+        {
+			// Test failed, print out error message
+			// "======" are there to make the failures more visible
+			System.out.println("=====================================================================");
+			System.out.println("[FAILED] #168: Merge Locations");
+			System.out.println("=====================================================================");
 			e.printStackTrace();
 		}
+        
+        /** Sprint 7 Test Cases **/
+        // TO DO: Implement Sprint 7 Test Cases
         
         
         // exit the program explicitly
